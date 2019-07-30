f3237b578a392e482801a2068338ff6b22c2dad4
Rodrigo Souza <rodrigo.souza.camelo@gmail.com>
2019-07-30 19:54:30 -0300

    Authentication
    Added login pages and main page and adding Authentication Function

---
+++ resources/views/templates/menu.blade.php
@@ -0,0 +1,16 @@
+<nav id="principal">
+	<ul>
+		<li>
+			<a>
+				<i class="fa fa-th-large"></i>
+				<h3>Página Inicial</h3>
+			</a>
+		</li>
+		<li>
+			<a>
+				<i class="fa fa-sign-out"></i>
+				<h3>Logout</h3>
+			</a>
+		</li>
+	<ul>
+</nav>
